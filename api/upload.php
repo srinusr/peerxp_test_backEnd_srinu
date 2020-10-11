466b0b17be551e5e7b2a1db0c04e331710574f7b
srinugodhala <g.srinivasulu@vassarlabs.com>
2020-10-11 20:23:32 +0530

    latest

--- api/upload.php
+++ api/upload.php
@@ -8,13 +8,13 @@ if(isset($postdata) && !empty($postdata))
 
 $ch = curl_init();
 
-curl_setopt($ch, CURLOPT_URL,"https://desk.zoho.in/api/v1/tickets");
+curl_setopt($ch, CURLOPT_URL,"https://desk.zoho.in/api/v1/uploads");
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS,$postdata);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     'Authorization: ca2e56a68d01e393c0e4c5aa5638729c',
     'orgId: 60001280952',
-    'Content-Type: application/json'
+    'Content-type: multipart/form-data'
   ));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
